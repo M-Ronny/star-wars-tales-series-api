7165d036ec80b667e28681f40a1a72a6d5760db0
Ronaldo <ronaldomorales.ai@gmail.com>
2026-03-05 16:18:14 -0500

    Added cors in server.js to handle cors errors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 3000
+
+app.use(cors())
 
 const tales = {
     'star wars: tales of the jedi': {
